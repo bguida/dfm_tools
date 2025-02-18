d025468b171c24d416ef6ad6a0fc6e89fa11b7fe
bguida <b.guida@ensometocean.com>
2025-02-18 17:45:20 +0100

    get_coastline_shp

--- dfm_tools/coastlines.py
+++ dfm_tools/coastlines.py
@@ -152,6 +152,7 @@ def get_coastlines_shp(dirshp:str = None, res:str='h', bbox:tuple = (-180, -90, 
     coastlines_gdb_L1 = gpd.read_file(file_shp_L1, columns=columns, where=f"AREA>{min_area}", bbox=bbox)
     coastlines_gdb_L6 = gpd.read_file(file_shp_L6, columns=columns, where=f"AREA>{min_area}", bbox=bbox)
     coastlines_gdb_list = [coastlines_gdb_L1,coastlines_gdb_L6]
+    print(coastlines_gdb_L1)
     # if len(coastlines_gdb_L1)<2: #if only one L1 polygon is selected, automatically add lakes and islands-in-lakes
         # coastlines_gdb_L2 = gpd.read_file(file_shp_L2, columns=columns, where=f"area>{min_area}", bbox=bbox)
         # coastlines_gdb_list.append(coastlines_gdb_L2)
