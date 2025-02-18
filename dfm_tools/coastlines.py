b5199cbb7af31e56dba63796308000b9fb6458a4
bguida <b.guida@ensometocean.com>
2025-02-18 17:59:44 +0100

    get_coastline_shp

--- dfm_tools/coastlines.py
+++ dfm_tools/coastlines.py
@@ -89,8 +89,8 @@ def get_coastlines_gdb(res:str='h', bbox:tuple = (-180, -90, 180, 90), min_area:
     
     print('>> reading coastlines: ',end='')
     dtstart = dt.datetime.now()
-    coastlines_gdb_L1 = gpd.read_file(file_shp_L1, where=f"area>{min_area}", bbox=bbox)
-    coastlines_gdb_L6 = gpd.read_file(file_shp_L6, where=f"area>{min_area}", bbox=bbox)
+    coastlines_gdb_L1 = gpd.read_file(file_shp_L1, columns=columns, where=f"area>{min_area}", bbox=bbox)
+    coastlines_gdb_L6 = gpd.read_file(file_shp_L6, columns=columns, where=f"area>{min_area}", bbox=bbox)
     coastlines_gdb_list = [coastlines_gdb_L1,coastlines_gdb_L6]
     if len(coastlines_gdb_L1)<2: #if only one L1 polygon is selected, automatically add lakes and islands-in-lakes
         coastlines_gdb_L2 = gpd.read_file(file_shp_L2, columns=columns, where=f"area>{min_area}", bbox=bbox)
@@ -149,10 +149,9 @@ def get_coastlines_shp(dirshp:str = None, res:str='h', bbox:tuple = (-180, -90, 
     
     print('>> reading coastlines: ',end='')
     dtstart = dt.datetime.now()
-    coastlines_gdb_L1 = gpd.read_file(file_shp_L1, columns=columns, where=f"AREA>{min_area}", bbox=bbox)
-    coastlines_gdb_L6 = gpd.read_file(file_shp_L6, columns=columns, where=f"AREA>{min_area}", bbox=bbox)
+    coastlines_gdb_L1 = gpd.read_file(file_shp_L1,where=f"AREA>{min_area}", bbox=bbox)
+    coastlines_gdb_L6 = gpd.read_file(file_shp_L6,where=f"AREA>{min_area}", bbox=bbox)
     coastlines_gdb_list = [coastlines_gdb_L1,coastlines_gdb_L6]
-    print(min_area)
     # if len(coastlines_gdb_L1)<2: #if only one L1 polygon is selected, automatically add lakes and islands-in-lakes
         # coastlines_gdb_L2 = gpd.read_file(file_shp_L2, columns=columns, where=f"area>{min_area}", bbox=bbox)
         # coastlines_gdb_list.append(coastlines_gdb_L2)
