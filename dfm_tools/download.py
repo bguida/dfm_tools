1894839a1e4b027c0084e8c63b5cd5efd465c7af
bguida <b.guida@ensometocean.com>
2025-04-29 20:01:34 +0200

    download MED do not write nc if empty

--- dfm_tools/download.py
+++ dfm_tools/download.py
@@ -319,9 +319,9 @@ def download_MED_CMEMS(varkey,
                 print(f'"{name_output}" found and overwrite=False, continuing.')
                 continue
             dataset_perperiod = dataset.sel(time=slice(date_str, date_str))
-            if dataset_perperiod.time != 0:
-                print(f'xarray writing netcdf file: {name_output}')
-                dataset_perperiod.to_netcdf(output_filename)
+            #if dataset_perperiod.time != 0:
+            print(f'xarray writing netcdf file: {name_output}')
+            dataset_perperiod.to_netcdf(output_filename)
 
 def copernicusmarine_get_product(date_min, date_max, vartype):
     assert vartype in ['phy','bio']
